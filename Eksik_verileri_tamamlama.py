35cc6c6efd4d9eee820bc4845f533ff0955cda3b
Kubra Ozden <kubraozden19@gmail.com>
2023-11-25 21:56:53 +0300

    Eksik veriler giderildi

---
+++ Eksik_verileri_tamamlama.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 23 15:58:26 2023
+
+@author: Lenovo
+"""
+#kütüphaneler
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+#veri yükleme
+veriler = pd.read_csv('eksikveriler.csv')
+print(veriler)
+
+#veri ön işleme
+boy = veriler[['boy']]
+print(boy)
+
+boykilo = veriler[["boy","kilo"]]
+print(boykilo)
+
+
+#eksik veriler
+
+from sklearn.impute import SimpleImputer
+
+#amacımız NaN olan değerleri tamamlamak 
+imputer = SimpleImputer(missing_values = np.nan, strategy = 'mean')
+# strategy ne ile impute edeceği yani değiştireceği 
+# ortamalama değerini bütün nan değerlere yazacağız
+
+Yas = veriler.iloc[:,1:4].values  # yas kolonunu çekmek için bütün satırlar ve 1. ve 4. kolonları çekiyoruz
+print(Yas)
+
+# Yas aslında bir dizi bu dizi üzerinde impute işlemini gerçekleştireceğiz
+
+imputer = imputer.fit(Yas[:,1:4])   
+
+# burada fit() fonksiyonumuz öğrenilecek olan değer.
+# fit() fonksiyonu eğitmek için kullanılır.
+# Yaşın 1 den 4 e kadar olan kolonlarını öğrenmesini söylüyoruz
+# sayısal kolonlar üzerinde imputer öğrenme işlemini yapacak
+# simple imputer daki strategy mean olduğu için bu kolonların ortalama değerlerini öğrenecek
+
+# öğrendikten sonra NaN değerleri dönüştürmesini istiyoruz bunun içinde transform fonksiyonunu kullanacağız
+# yani fit'le öğretip transformla da öğrendiğini uygulamasını sağlıyoruz
+# öğrenilen şey eksik değerlerin yerine konulacak olan değer yani ort değer
+# uygulanacak olan şey de nan değerlerin değiştirilmesi
+Yas[:,1:4] = imputer.transform(Yas[:,1:4])
+print(Yas)
+
+
+
+
+
+
+
+
+
+
+
